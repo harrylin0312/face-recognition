ba35382826522c322608d4b8989a9bd88dd01ef8
Hengyu Lin <harrylinpm1145@gmail.com>
2025-06-23 14:51:44 +0800

    Update camera.js

--- start/camera.js
+++ start/camera.js
@@ -39,7 +39,7 @@ export async function startCamera() {
         toggleSection("eventDetail", window.currentEventId);
         return;
     }
-    socket = new WebSocket("wss://not-regard-dogs-fewer.trycloudflare.com");
+    socket = new WebSocket("wss://hostels-wilderness-chrome-accent.trycloudflare.com");
 
     socket.onopen = () => {
         console.log("🔌 WebSocket 已連線 (startCamera)");
