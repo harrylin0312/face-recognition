d15ed06129f39f782b43539a3d8b70fdb2d5c109
Hengyu Lin <harrylinpm1145@gmail.com>
2025-06-30 01:35:21 +0800

    Update script1.js

--- login/script1.js
+++ login/script1.js
@@ -181,7 +181,7 @@ function login() {
                 container.classList.add("expand");
 
                 setTimeout(() => {
-                    window.location.href = "https://harrylin0312.github.io/face-recognition/start/";
+                    window.location.href = "../start/";
                 }, 1500);
             }, 1000);
         })
@@ -251,7 +251,7 @@ document.getElementById("prompt").addEventListener("click", function () {
         });
 
         promptIcon.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="24" height="24">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="black" width="24" height="24">
                 <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
             </svg>
         `;
